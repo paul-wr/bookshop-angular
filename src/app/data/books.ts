db0554dc7efafb5ddbd484e105f5ffd1377da178
Paul Wr <paul.w35@live.com>
2017-11-19 17:48:17 +0000

    jQuery implemented

--- src/app/data/books.ts
+++ src/app/data/books.ts
@@ -61,5 +61,159 @@ export const books = [
     "category" : "education",
     "description": "CONTEMPORARY MUSIC EDUCATION explores the theory and practice of teaching music by placing it in the broader context of culture and history. This core text offers music education students a practical and rigorous overview of the profession, covering curriculum development, assessment, and advocacy, while examining the changes brought about by technology, social justice movements, and a half-century of educational reforms.",
     "url":"https://www.easons.com/globalassets/5637150827/all/books/education/teachers-reference-teaching-skills-aids/9781133956303.jpg?"
+  },
+  {
+    "id": 8,
+    "title": "Maths",
+    "author": "June Haighton",
+    "cost": "27.30",
+    "category" : "education",
+    "description": "Building on the popularity of our best-selling Maths the Basic Skills, this new edition is ideal for those who need to achieve the minimum level of Functional Skills required for employment or want to help their children with their homework or would simply just like to brush up on their maths skills.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/science-and-nature/science/mathematics/9781408521120.jpg?"
+  },
+  {
+    "id": 9,
+    "title": "Geography",
+    "author": "Garrett Nagle",
+    "cost": "33.59",
+    "category" : "education",
+    "description": "This Study Guide supports the new syllabus for 2009 and includes all options. Written by experienced IB teachers and workshop leaders, it contains all the information required for the course, presented in a concise, accessible format. The book exactly matches the structure of the new course and includes material for both Standard and Higher level. Questions and full mark schemes are provided along with advice on internal and external assessment. The Study Guide may be used by teachers or to support the students.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/history-and-politics/current-affairs/environment/9780198389156.jpg?"
+  },
+  {
+    "id": 9,
+    "title": "DIPLCM ELECTRIC GUITAR PERFORMANCE DIPLOMA HANDBOOK",
+    "author": "Tony Skinner",
+    "cost": "18.19",
+    "category" : "music",
+    "description": "Book & CD. Use this book to develop your playing and gain an internationally-recognised qualification in electric guitar playing. The book includes a free CD, including guitar solos of Carlos Santana, Gary Moore and Joe Satriani, as well as demonstration tracks and backing tracks. ",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/entertainment-and-music/entertainment-books/musicianship/9781905908219.jpg?"
+  },
+  {
+    "id": 10,
+    "title": "RGT ROCK GUITAR PLAYING -- GRADE EIGHT",
+    "author": "Tony Skinner",
+    "cost": "13.99",
+    "category" : "music",
+    "description": "Compiled by the Registry of Guitar Tutors -- the specialists in guitar education. Use this book to develop your playing and gain a recognised qualification in rock guitar playing. This book focuses on the Improvisation and Aural Assessment sections of the RGT Grade Eight exam in rock guitar playing. ",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/entertainment-and-music/entertainment-books/musicianship/9781905908387.jpg?"
+  },
+  {
+    "id": 11,
+    "title": "PIANO MANUAL",
+    "author": "Graham Barker",
+    "cost": "27.99",
+    "category" : "music",
+    "description": "How a piano works. How to buy wisely, both new and used - there are lots of terrible pianos out there! How to look after your piano, from simple routine maintenance to complex repairs. How to tune your own piano. J.S.Bach's equal temperament tuning innovation clearly explained - of interest to all musicians.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/entertainment-and-music/entertainment-books/musicianship/9781844254859.jpg?"
+  },
+  {
+    "id": 13,
+    "title": "HEALTH HACKS",
+    "author": "Esme Floyd",
+    "cost": "11.20",
+    "category" : "health",
+    "description": "Would you like to know how to reduce pain without medication? Why it's easier to lose weight if you eat grapefruit? Or why oatmeal with milk will help you get a good night's sleep? These handy little hacks will help you to make small changes that reap really big health benefits. With direct remedies for health problems as well as preventative advice on topics ranging from nutrition and fitness to illnesses and stress, the book lets you understand why your body works the way it does, how you can remedy it when it's underperforming and how you can keep it a well-oiled machine all through life.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/body-and-soul/health/family-health/9781780977034.jpg?"
+  },
+  {
+    "id": 14,
+    "title": "HEALTH AND MEDICAL GEOGRAPHY",
+    "author": "Michael Emch",
+    "cost": "68.59",
+    "category" : "health",
+    "description": "Why are rainfall, carcinogens, and primary care physicians distributed unevenly over space? The fourth edition of the leading text in the field has been updated and reorganized to cover the latest developments in disease ecology and health promotion across the globe. The book accessibly introduces the core questions and perspectives of health and medical geography and presents cutting-edge techniques of mapping and spatial analysis. ",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/education/medical-academic/9781462520060.jpg?"
+  },
+  {
+    "id": 15,
+    "title": "MENTAL HEALTH MATTERS",
+    "author": "Paula Nagel",
+    "cost": "26,59",
+    "category" : "health",
+    "description": "Teachers have a responsibility to support the mental health of children in their care. Current statistics show a significant rise in mental health difficulties in children and young people, and new legislation urges schools to consider whether continuing disruptive behaviour might be the result of an unmet need. However, this is not an area that is universally addressed in teacher training programmes or books. Using real life case studies, this book supports all teachers and school staff in understanding and identifying the early signs of mental health difficulties, and explains how to bring about appropriate early interventions.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/education/sociology-academic/9781472921406.jpg?"
+  },
+  {
+    "id": 16,
+    "title": "NATURE",
+    "author": "Ralph Waldo Emerson",
+    "cost": "6,99",
+    "category" : "nature",
+    "description": "Originally published anonymously, Nature was the first modern essay to recommend the appreciation of the outdoors as an all-encompassing positive force. Emerson's writings were recognized as uniquely American in style and content, and launched the idea of going for a walk as a new way of looking at the world. Throughout history, some books have changed the world. They have transformed the way we see ourselves - and each other. They have inspired debate, dissent, war and revolution. They have enlightened, outraged, provoked and comforted. They have enriched lives - and destroyed them. Now Penguin brings you the works of the great thinkers, pioneers, radicals and visionaries whose ideas shook civilization and helped make us who we are.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/fiction/literature/literary-reference--essays/9780141036823.jpg?"
+  },
+    {
+    "id": 17,
+    "title": "ENVIRONMENTAL AND NATURE WRITING",
+    "author": "Dr Sean Prentiss",
+    "cost": "30.79",
+    "category" : "nature",
+    "description": "Offering guidance on writing poetry, nonfiction, and fiction, Environmental and Nature Writing is a complete introduction to the art and craft of writing about the environment in a wide range of genres. With discussion questions and writing prompts throughout, Environmental and Nature Writing: A Writers' Guide and Anthology covers such topics as: · The history of writing about the environment · Image, description and metaphor · Environmental journalism, poetry, and fiction · Researching, revising and publishing · Styles of nature writing, from discovery to memoir to polemic The book also includes an anthology, offering inspiring examples of nature writing in all of the genres covered by the book, including work by: John Daniel, Camille T. Dungy, David Gessner, Jennifer Lunden, Erik Reece, David Treuer, Bonnie Jo Campbell, Alyson Hagy, Bonnie Nadzam, Lydia Peelle, Benjamin Percy, Gabrielle Calvocoressi, Nikky Finney, Juan Felipe Herrera, Major Jackson, Aimee Nezhukumatathil, G.E. Patterson, Natasha Trethewey, and many more.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/fiction/literature/literary-reference--essays/9781472592538.jpg?"
+  },
+  {
+    "id": 18,
+    "title": "GLOBAL WARMING",
+    "author": "John Houghton",
+    "cost": "48.99",
+    "category" : "nature",
+    "description": "How much of global warming is due to human activities? How far will it be possible to adapt to changes of climate? Sir John Houghton's definitive, full colour guide to climate change answers these questions and more by providing the best and latest information available, including the latest IPCC findings. The simple, logical flow of ideas gives an invaluable grounding in the science, as well as the physical and human impacts of climate change, for undergraduate students across a wide range of disciplines. Accessible to both scientists and non-scientists, the text avoids mathematical equations and includes more technical material in boxes, while simple figures help students to understand the conclusions the science leads to without being overwhelmed by vast amounts of data. Questions for students to consider and test their understanding are included in each chapter, along with carefully selected further reading to expand their knowledge.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/education/engineering-and-construction-academic/9781107463790.jpg?"
+  },
+   {
+    "id": 19,
+    "title": "MIND BODY BABY",
+    "author": "Ann Bracken",
+    "cost": "14.00",
+    "category" : "mind body spirit",
+    "description": "Hospital visits, calendar watching, disheartening test results and scheduled sex can make trying for a baby a stressful experience, and the strain it puts on couples and individuals struggling with infertility can impact hugely on the likelihood of successful conception. With an approach that puts wellbeing at the heart of fertility, this easy-to-follow guide by top fertility specialist Ann Bracken offers a proven successful alternative to formal counselling. Her comprehensive and supportive book gives help and guidance on how to enhance and improve the outcome",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/body-and-soul/parenting/pregnancy-childbirth--breastfeeding/97814736204212.jpg?"
+  },
+  {
+    "id": 20,
+    "title": "THE BODY HAS A MIND OF ITS OWN",
+    "author": "Sandra Blakeslee",
+    "cost": "19.59",
+    "category" : "mind body spirit",
+    "description": "Your body has a mind of its own. You know it's true. You can sense it, even though it may be hard to articulate. You know that your body is more than a vehicle for your brain to cruise around in, but how deeply are mind and body truly interwoven? Answers can be found in the emerging science of body maps. Just as road maps represent interconnections across the landscape, your many body maps represent all aspects of your bodily self. Your self doesn't begin and end with your physical body but extends into the space around you. When you drive a car, your personal body space grows to envelop it. When you play a video game, your body maps automatically track and emulate the actions of your character onscreen.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/body-and-soul/psychology/popular-psychology/9780812975277.jpg?"
+  },
+  {
+    "id": 21,
+    "title": "YOUR BODY SPEAKS YOUR MIND",
+    "author": "Deb Shapiro",
+    "cost": "21.00",
+    "category" : "mind body spirit",
+    "description": "Fully updated and expanded edition of the classic book on body-mind. What are your symptoms and illnesses telling you about yourself? In Your Body Speaks Your Mind, renowned teacher and bestselling author Deb Shapiro shows how understanding your body's 'language of symptoms' can increase your potential for healing. She explains the interconnectedness between your physical state and your emotional, psychological and spiritual health, and reveals: How unresolved emotional and psychological issues can affect your physical health; How feelings and thoughts are linked to specific parts of the body; How you can take steps to heal your body with your mind, and your mind with your body. Your Body Speaks Your Mind shows you how to initiate communication between body and mind, and decode the priceless information your body is giving you, in order to achieve better health and a greater sense of wellbeing.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/body-and-soul/parenting/pregnancy-childbirth--breastfeeding/97814736204212.jpg?"
+  },
+  {
+    "id": 22,
+    "title": "COLLINS GERMAN",
+    "author": "Collins Dictionary",
+    "cost": "21.55",
+    "category" : "language",
+    "description": "UK's bestselling language learning audio series This German Audio Course is the perfect companion for holidays and business trips in Germany and German-speaking countries. Easy to use - fast, flexible and portable Easy to follow - just listen, speak and learn Easy to learn - no complex grammar Easy Learning German teaches you key words for the most important situations and lets you expand your language through a variety of activities. You choose whether to focus on learning the essentials or to progress to some more challenging activities - so whether you're a beginner, or you want to refresh and build upon your existing knowledge, you can become confident in German without even having to pick up a pen. Contains 1 booklet and 3 CDs with over 3.5 hours of audio.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/education/language/tefl-and-language/9780008205706.jpg?width=270&height=360&mode=max"
+  },
+  {
+    "id": 23,
+    "title": "COLLINS SPANISH DICTIONARY & GRAMMAR",
+    "author": "Collins Dictionary",
+    "cost": "18.20",
+    "category" : "language",
+    "description": "An up-to-date dictionary and a user-friendly grammar guide in one handy volume. A clear layout, cultural notes, and an easy-to-use grammar section make this book ideal for intermediate learners. Designed for intermediate learners of Spanish, whether at school, at home, or for business. 80,000 references and 110,000 translations will help those learning Spanish take their language skills to the next level. This edition has been revised and updated to offer extensive and relevant coverage of today's English and Spanish, with thousands of phrases and examples guiding the user to the most appropriate translation. A comprehensive grammar guide which presents detailed examples and translations to help users understand Spanish grammar - the perfect complement to the dictionary. The clear Collins typography gives the text a contemporary feel, and along with the new alphabet tabs, ensures that users find the information they need quickly and easily.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/miscellaneous-books/9780007484362.jpg?"
+  },
+   {
+    "id": 24,
+    "title": "COLLINS EASY LEARNING IRISH GRAMMAR",
+    "author": "Collins Dictionary",
+    "cost": "12.60",
+    "category" : "language",
+    "description": "This grammar of Irish offers beginners a clear and easy-to-understand guide to help with Irish language study. For all those learning Irish at school, work or home this book provides easily accessible information in an attractively presented layout. Key grammatical points are highlighted throughout as a means of reinforcement. In addition, a full glossary gives clear explanations of grammatical terminology. All the regular verb forms and conjugations, and the most common tenses of irregular verbs are shown in full. An index contains hundreds of verbs which are cross-referred to their conjugation model. Hundreds of examples of real Irish illustrate clearly the grammatical points being made.",
+    "url":"https://www.easons.com/globalassets/5637150827/all/books/fiction/fiction-a-to-z/foreign-language-titles/97800082070452.jpg?"
   }
 ]
+
