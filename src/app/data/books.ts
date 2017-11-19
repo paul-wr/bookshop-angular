7163059bda8c42a798cc83d82edd857f7ebcedcd
Paul Wr <paul.w35@live.com>
2017-11-19 18:40:21 +0000

    Styles Updated

--- src/app/data/books.ts
+++ src/app/data/books.ts
@@ -26,33 +26,7 @@ export const books = [
     "description": "Quickly discover solutions to common problems, best practices you can follow, and everything JavaScript has to offer. Using a problem-solution approach, this book takes you from language basics like built-in objects and flow control all the way to advanced optimization techniques, frameworks and Node.js. With JavaScript Recipes you will learn language fundamentals like types, conversions, execution contexts, expressions, operators, statements, and built-in objects.",
     "url":"https://www.easons.com/globalassets/5637150827/all/books/business-computers/computers/computer-programming/9781430261063.jpg?"
   },
-  {
-    "id": 4,
-    "title": "Java",
-    "author": "Herbert Schildt",
-    "cost": "58.80",
-    "category" : "computer science",
-    "description": "The Definitive Java Programming Guide Fully updated for Java SE 8, Java: The Complete Reference , Ninth Edition explains how to develop, compile, debug, and run Java programs. Bestselling programming author Herb Schildt covers the entire Javalanguage, including its syntax, keywords, and fundamental programming principles, as well as significant portions of the Java API library.",
-    "url":"https://www.easons.com/globalassets/5637150827/all/books/business-computers/computers/computer-programming/9780071808552.jpg?"
-  },
-  {
-    "id": 5,
-    "title": "Java",
-    "author": "Herbert Schildt",
-    "cost": "58.80",
-    "category" : "computer science",
-    "description": "The Definitive Java Programming Guide Fully updated for Java SE 8, Java: The Complete Reference , Ninth Edition explains how to develop, compile, debug, and run Java programs. Bestselling programming author Herb Schildt covers the entire Javalanguage, including its syntax, keywords, and fundamental programming principles, as well as significant portions of the Java API library.",
-    "url":"https://www.easons.com/globalassets/5637150827/all/books/business-computers/computers/computer-programming/9780071808552.jpg?"
-  },
-  {
-    "id": 6,
-    "title": "Java",
-    "author": "Herbert Schildt",
-    "cost": "58.80",
-    "category" : "computer science",
-    "description": "The Definitive Java Programming Guide Fully updated for Java SE 8, Java: The Complete Reference , Ninth Edition explains how to develop, compile, debug, and run Java programs. Bestselling programming author Herb Schildt covers the entire Javalanguage, including its syntax, keywords, and fundamental programming principles, as well as significant portions of the Java API library.",
-    "url":"https://www.easons.com/globalassets/5637150827/all/books/business-computers/computers/computer-programming/9780071808552.jpg?"
-  },
+  
   {
     "id": 7,
     "title": "Contemporary Music Education",
